257ea2bde0922983baf218fcdb8446822ebda6e6
Raj_Dash <rajdash3472@gmail.com>
2022-08-05 12:25:11 +0600

    Array Part Added

--- practise-module-18.js
+++ practise-module-18.js
@@ -2,11 +2,13 @@
 
 // Ans: JavaScript is the Programming Language for the Web.JavaScript can update and change both HTML and CSS.JavaScript can calculate, manipulate and validate data.
 
+
 // ২. জাভাস্ক্রিপ্ট কিভাবে কাজ করে সেটা কি জানো ?
 
 // Ans : JavaScript is a scripting language.It is designed with the goal of behavior control at the client level of the website i.e.the browser.But today it runs on any operating system apart from the browser and has all the features of other high level programming languages.
 
 // উত্তরঃ জাভাস্ক্রিপ্ট একটি স্ক্রিপ্টিং ভাষা। এটি ওয়েবসাইটের ক্লায়েন্ট স্তরে অর্থাৎ ব্রাউজারে আচরণ নিয়ন্ত্রণের লক্ষ্য নিয়ে ডিজাইন করা হয়েছে। কিন্তু আজ এটি ব্রাউজার ব্যতীত যেকোনো অপারেটিং সিস্টেমে চলে এবং অন্যান্য উচ্চ - স্তরের প্রোগ্রামিং ভাষার সমস্ত বৈশিষ্ট্য রয়েছে।
+
 
 // ৩. ভেরিয়েবল কি জিনিস ?
 
@@ -14,9 +16,11 @@
 
 // উত্তরঃ পরিবর্তনশীল মানে যা পরিবর্তিত হতে পারে। জাভাস্ক্রিপ্টে, একটি ভেরিয়েবল ডেটা মান সংরক্ষণ করে যা পরে পরিবর্তন করা যেতে পারে। জাভাস্ক্রিপ্টে একটি পরিবর্তনশীল ঘোষণা করতে সংরক্ষিত কীওয়ার্ড var ব্যবহার করুন।
 
+
 // ৪. ভেরিয়েবল কিভাবে ডিক্লেয়ার করে ?
 
 // Ans: var numbar = 15;
+
 
 // ৫. ভেরিয়েবল এর মান কিভাবে চেইঞ্জ করে বা আপডেট করে ?
 
@@ -24,6 +28,7 @@
 // var age = 35;
 // age = 45;
 // console.log(age);
+
 
 // ৬. কি কি ধরণের ভেরিয়েবল হয়। সেগুলা কি কি(হিন্টস: Numeric, String, Boolean)
 // Ans:
@@ -38,6 +43,7 @@
 // Arrays
 // Classes
 
+
 // ৭. জাভাস্ক্রিপ এ primitive and non primitive data types কি কি ? উদাহরণ হিসেবে বলো।
 // Ans:
 // Primitive Datatypes:
@@ -50,6 +56,7 @@
 // Strings
 // Arrays
 // Classes
+
 
 // ৮. ভেরিয়েবল এর নাম কিভাবে কিভাবে ডিক্লেয়ার করতে হয়। কোন কোন জিনিস নাম এ লেখা যাবে না। অর্থাৎ Variable এর naming convention সম্পর্কে বলো।
 
@@ -64,6 +71,7 @@
 // ১/ Word Gap দেওয়া যাবে না ।
 // ২/ "" দেওয়া যাবে না ।
 // ৩/ সামনে সংখ্যা দেওয়া যাবে না ।
+
 
 // ৯. দুইটা ভেরিয়েবল এর মধ্যে =, -, *, /, % কিভাবে করে?
 // 1/ =,
@@ -95,6 +103,7 @@
 // var num2 = 10;
 // var total = num1 % num2;
 // console.log(total);
+
 
 // ১০. শর্টহ্যান্ড গুলো জানতে হবে। বিশেষ করে +=, -=, *=, /= জানতে হবে।
 
@@ -187,30 +196,146 @@
 
 //     --------------
 
-// ১৩. Array কি জিনিস। এইটা কিভাবে কাজ করে ? কিভাবে Array ডিক্লেয়ার করতে হয় 
+// ১৩. Array কি জিনিস। এইটা কিভাবে কাজ করে ? কিভাবে Array ডিক্লেয়ার করতে হয়
+// Ans:
+// An array can hold many values under a single name, and you can access the values by referring to an index number.
 
-// ১৪. array এর মধ্যে কয়টা উপাদান(element) আছে সেটা কিভাবে বের করে 
+// একটি অ্যারে একটি একক নামের অধীনে অনেকগুলি মান ধরে রাখতে পারে এবং আপনি একটি সূচক নম্বর উল্লেখ করে মানগুলি অ্যাক্সেস করতে পারেন।
 
-// ১৫. array এর উপাদান গুলা এর পজিশন(index) কিভাবে কাজ করে। কত দিয়ে শুরু হয়। এবং মান কিভাবে চেইঞ্জ হয়। 
 
-// ১৬. কোন একটা উপাদানের index এর মান -১ বলতে কি বুঝায় 
+// ১৪. array এর মধ্যে কয়টা উপাদান(element) আছে সেটা কিভাবে বের করে
+// Ans:
+// var friendsAge = [42, 44, 14, 63, 54, 45];
+// console.log(friendsAge.length);
 
-// ১৭. কিভাবে index দিয়ে কোন একটা array এর মধ্যে উপাদান এর মান খুঁজে বের করতে পারো 
 
-// ১৮. কিভাবে কোন একটা index পজিশন এ array এর উপাদান এর মান চেইঞ্জ করতে পারবে 
+// ১৫. array এর উপাদান গুলা এর পজিশন(index) কিভাবে কাজ করে। কত দিয়ে শুরু হয়। এবং মান কিভাবে চেইঞ্জ হয়।
+// Ans:
+// Array এর index- "0" থেকে শুরু হয়
 
-// ১৯. একটা Array এর মধ্যে কোন একটা উপাদান এর মান তোমাকে দেয়া আছে এখন সেটার index তুমি কিভাবে খুঁজে বের করতে পারো ?
+// Get index by the element
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// var indexOfFrinedsName = friendsName.indexOf('Roni');
+// console.log(indexOfFrinedsName);
+
+
+// ১৬. কোন একটা উপাদানের index এর মান -১ বলতে কি বুঝায়
+// Ans:
+// কোন একটা উপাদানের index এর মান -১ বলতে বুঝায় ঐ উপাদানটি Array এর মধ্যে নাই।
+
+
+// ১৭. কিভাবে index দিয়ে কোন একটা array এর মধ্যে উপাদান এর মান খুঁজে বের করতে পারো
+// Ans:
+// Find the position of element by using index
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// console.log(friendsName[2]);
+
+
+// ১৮. কিভাবে কোন একটা index পজিশন এ array এর উপাদান এর মান চেইঞ্জ করতে পারবে
+// Ans:
+// Change the element by using index
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// friendsName[2] = 'Megha';
+// console.log(friendsName);
+
+// ১৯. একটা Array এর মধ্যে কোন একটা উপাদান এর মান তোমাকে দেয়া আছে এখন সেটার index তুমি   কিভাবে খুঁজে বের করতে পারো ?
+// Ans:
+// Get index by the element
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// var indexOfFrinedsName = friendsName.indexOf('Roni');
+// console.log(indexOfFrinedsName);
+
 
 // ২০.ধরো, কোন একটা ইনডেক্স দিয়ে উপাদান খুঁজতে গেছো। কিন্তু সেটার মান না দিয়ে তোমাকে undefined দেখাচ্ছে। সেটা দেখে তুমি কি বুঝবে ? (একটু গুগলে সার্চ দাও। আমরা কোর্স এ এইটা আলোচনা করিনি। তারপরেও চেষ্টা করে দেখো)
 
 // ২১. কোন একটা Array এর মধ্যে লাস্ট উপাদান হিসেবে কোন উপাদান হিসেবে যোগ করতে চাইলে কিভাৱে যোগ করবে। আবার Array থেকে শেষের উপাদান টা বের করে দিতে চাইলে কিভাবে বের করে দিবে
+// Ans:
+// Added the last element
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// friendsName.push('Megha');
+// console.log(friendsName);
+
+// Remove the last element
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// friendsName.pop();
+// console.log(friendsName);
+
 
 // ২২. কোন একটা Array এর মধ্যে প্রথম উপাদান হিসেবে কোন উপাদান হিসেবে যোগ করতে চাইলে কিভাৱে যোগ করবে। আবার Array থেকে প্রথম উপাদান টা বের করে দিতে চাইলে কিভাবে বের করে দিবে
+// Ans:
+// Added the last element
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// friendsName.unshift('Megha');
+// console.log(friendsName);
 
-// ২৩. তুলনা কিভাবে করতে হয়। এইগুলার মানে কি: >, <, ==, !=, <=, >=, ===, !==, &&, ।। 
+// Remove the last element
+// var friendsName = ['Joy', 'Roni', 'Sumi', 'Mitu', 'Ritu'];
+// friendsName.shift();
+// console.log(friendsName);
 
-// ২৪. তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে, ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে।
 
+// ২৩. তুলনা কিভাবে করতে হয়। এইগুলার মানে কি: >, <, ==, !=, <=, >=, ===, !==, &&, ।।
+// Ans:
+// var num1 = 10;
+// var num2 = 12;
+// var result = num1 > num2;
+// console.log(result);
+
+// var num1 = 10;
+// var num2 = 12;
+// var result = num1 < num2;
+// console.log(result);
+
+// var num1 = 10;
+// var num2 = 12;
+// var result = num1 == num2;
+// console.log(result);
+
+// var num1 = 10;
+// var num2 = 12;
+// var result = num1 != num2;
+// console.log(result);
+
+// var num1 = 10;
+// var num2 = 12;
+// var result = num1 <= num2;
+// console.log(result);
+
+// var num1 = 10;
+// var num2 = 12;
+// var result = num1 >= num2;
+// console.log(result);
+
+// var num1 = '10';
+// var num2 = 12;
+// var result = num1 === num2;
+// console.log(result);
+
+// var num1 = '10';
+// var num2 = 12;
+// var result = num1 !== num2;
+// console.log(result);
+
+
+// ২৪. তোমার কাছে: ৮০,০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০,০০০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০,০০০ টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে, ২০,০০ টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে।
+// Ans:
+// var money = 35000;
+
+// if (money >= 80000) {
+//     console.log("I will buy a Mac !!");
+// }
+// else if (money >= 60000) {
+//     console.log("I will buy a Gaming Laptop !!");
+// }
+// else if (money >= 40000) {
+//     console.log("I will buy a Lenovo Yoga !!");
+// }
+// else if (money >= 20000) {
+//     console.log("I will buy a old Laptop !!");
+// }
+// else {
+//     console.log("I will buy a Mobile Phone !!");
+// }
 
 
 // ---------------------
